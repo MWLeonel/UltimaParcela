0cbc5169a53fcf6696356a8135b139e18eb3e6cf
MWLeonel <wmleonel6@gmail.com>
2020-09-25 00:50:14 -0300

    Calculo do Tempo de Parcelas

---
+++ UltimaParcela/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorTotal;
+            double parcelas;
+            double result;
+
+            DateTime dataAtual = DateTime.Now;
+            DateTime parcelaFinal;
+
+            Console.Write("Digite o Valor Total: ");
+            valorTotal = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Numero de Parcelas: ");
+            parcelas = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            result = valorTotal / parcelas;
+
+            parcelaFinal = dataAtual.AddMonths(8);
+            Console.WriteLine($"Sua ultima parcela será em: {parcelaFinal}, no valor de R${result}");
+        }
+    }
+}
